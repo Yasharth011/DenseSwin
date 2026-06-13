a8be067293e99a6a2550848f76fda753abf1e4dd
Yasharth011 <yasharthgupta10@gmail.com>
2026-06-14 03:05:09 +0530

    removed backbone.py

--- models/backbone.py
+++
@@ -1,9 +0,0 @@
-import timm
-
-swin = timm.create_model(
-        'swin_tiny_patch4_window7_224',
-        pretrained = True,
-        features_only = True,
-        out_indices = (-1,) # c5 final layer
-        )
-swin.eval()

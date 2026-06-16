696a880cbab4d46765a2b2ee7644ed505f878cc2
Yasharth011 <yasharthgupta10@gmail.com>
2026-06-16 23:54:50 +0530

    density_module: shape alignment for video swin transformer and sigmoid for masking

--- models/density_module.py
+++ models/density_module.py
@@ -1,5 +1,7 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 
 def conv_bn_reul(in_ch, out_ch):
     return nn.Sequential(
@@ -8,12 +10,14 @@ def conv_bn_reul(in_ch, out_ch):
         nn.ReLU(inplace=True),
     )
 
+
 class DensityConv(nn.Module):
     def __init__(self, in_ch: int = 768):
         super().__init__()
         self.conv_blocks = nn.Sequential(
             conv_bn_reul(in_ch, 512), conv_bn_reul(512, 256), conv_bn_reul(256, 256)
         )
+        self.conv_align = conv_bn_reul(256, 768)
         self.density_head = nn.Conv2d(256, 1, kernel_size=1)
 
     def forward(self, swin3d_features, target_size):
@@ -21,13 +25,15 @@ class DensityConv(nn.Module):
         B, T, H, W, C = swin3d_features.shape
         swin3d_features = swin3d_features.permute(0, 2, 1, 3, 4)
         flattened_batch = swin3d_features.reshape(B * T, C, H, W)
-        F_dm = self.conv_blocks(flattened_batch)
+        features = self.conv_blocks(flattened_batch)
+        F_dm = self.conv_align(features)
+        F_dm = torch.sigmoid(F_dm) # activation function
 
-        features = F.interpolate(
-            F_dm, size=target_size, mode="bilinear", align_corners=False
+        interpolated_features = F.interpolate(
+            features, size=target_size, mode="bilinear", align_corners=False
         )
 
-        D = self.density_head(features)
+        D = self.density_head(interpolated_features)
         D = F.relu(D)
 
         F_dm = F_dm.view(B, T, 256, H, W)

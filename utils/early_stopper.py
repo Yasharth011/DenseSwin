f6ced4d7f8702560a4b2950e21093d3477f6f90b
Yasharth011 <yasharthgupta10@gmail.com>
2026-06-25 01:58:20 +0530

    early_stopper

---
+++ utils/early_stopper.py
@@ -0,0 +1,17 @@
+class EarlyStopper:
+
+    def __init__(self, patience=1, min_delta=0):
+        self.patience: int = patience
+        self.min_delta: float = min_delta
+        self.counter: int = 0
+        self.min_vloss: float = float("inf")
+
+    def early_stop(self, vloss: float) -> bool:
+        if vloss < self.min_vloss:
+            self.min_vloss = vloss
+            self.counter = 0
+        elif vloss > (self.min_vloss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False

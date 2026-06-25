42df8429b81c156ad594692508676d6ab7544046
Yasharth011 <yasharthgupta10@gmail.com>
2026-06-26 03:27:33 +0530

    Regression Metrics calculator class

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,7 @@
 from .loader import TrafficDensityDataset
 from .config import TRAIN_DATASET, TEST_DATASET, MODEL_CONFIG
 from .early_stopper import EarlyStopper
+from .metrics import RegressionMetrics
 
 __all__ = [
     "TrafficDensityDataset",
---
+++ utils/metrics.py
@@ -0,0 +1,43 @@
+import torch 
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+class RegressionMetrics:
+    """
+    Calulcate Regression Metrics : MAE, MSE and R^2 Error 
+    """
+    def __init__(self):
+        self.predictions = []
+        self.truths = []
+
+    def update(self, prediction: torch.Tensor, truth: torch.Tensor | int):
+
+        p = prediction.detach().cpu().squeeze().numpy().tolist()
+        
+        t = np.array([])
+        if isinstance(truth, torch.Tensor):
+            t = truth.detach().cpu().squeeze().numpy().tolist()
+        elif isinstance(truth, int): 
+            t = list(t)
+        
+        self.predictions.append(p)
+        self.targets.append(t)
+
+    def compute(self):
+
+        if not self.predictions:
+            return 0.0, 0.0, 0.0
+            
+        p = np.array(self.predictions)
+        t = np.array(self.targets)
+        
+        mae = mean_absolute_error(t, p)
+        mse = mean_squared_error(t, p)
+        r2 = r2_score(t, p)
+        
+        return mae, mse, r2
+        
+    def reset(self):
+
+        self.predictions = []
+        self.targets = []

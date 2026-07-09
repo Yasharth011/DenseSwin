0978517edc9115b33541e80c7fbf8e77f59c6307
Yasharth011 <yasharthgupta10@gmail.com>
2026-07-09 15:49:47 +0530

    train: only loading backbone and density_head from checkpoints

--- train/ucsd.py
+++ train/ucsd.py
@@ -36,6 +36,15 @@ CHECKPOINT = str(args.checkpoint)
 LR = float(args.learning_rate)
 DECAY = float(args.decay)
 
+checkpoint = torch.load(
+    os.path.join(MODEL_CONFIG.checkpoints, CHECKPOINT), weights_only=True
+)
+filtered_checkpoint = {
+    key: value
+    for key, value in checkpoint.items()
+    if key.startswith("backbone.") or key.startswith("density_head.")
+}
+
 transform = v2.Compose(
     [
         v2.Resize((224, 384)),
@@ -76,11 +85,8 @@ for fold in range(4):
 
     print(f"FOLD {fold} : ")
 
-    checkpoint = torch.load(
-        os.path.join(MODEL_CONFIG.checkpoints, CHECKPOINT), weights_only=True
-    )
     model = DenseSwin(num_class=3)
-    model.load_state_dict(checkpoint)
+    model.load_state_dict(filtered_checkpoint, strict=False)
     model.to(device=device)
 
     # freeze back and density head
